c5ae34fe4b50739f01724cfb024ba77793290709
Scott Owades <jeremyscottowades@gmail.com>
2020-12-01 15:50:46 -0800

    mapping & tooltips

--- make_smaller_file.py
+++ make_smaller_file.py
@@ -1,8 +1,10 @@
 import geopandas as gpd
 
 # parcels = gpd.read_file("https://opendata.arcgis.com/datasets/b55c25ae04fc47fc9c188dbbfcd51192_0.geojson")
-parcels = gpd.read_file('Parcels.geojson')
-# parcels = gpd.read_file('parcels_94608.geojson')
+# parcels = gpd.read_file('Parcels.geojson')
+parcels = gpd.read_file('parcels_94608.geojson')
 is_neighbor = parcels['SitusZip'] == "94608"
-parcels_local = parcels[is_neighbor]
-parcels_local.to_file("parcels_94608.geojson", driver='GeoJSON')
+parcels = parcels[is_neighbor]
+is_owned = parcels['MailingAddress'] == "1333 PARK AVE EMERYVILLE CA 94608"
+parcels = parcels[is_owned]
+parcels.to_file("parcels_small.geojson", driver='GeoJSON')
