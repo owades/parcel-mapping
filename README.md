60acab22764f6d03f46b9a4add80309449eeace2
Scott Owades <jeremyscottowades@gmail.com>
2020-12-01 11:16:00 -0800

    added comments

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # parcel-mapping
+
 Mapping parcels in Alameda County
+
+You're obligated to listen to the band Parcels while looking at this: https://open.spotify.com/album/6tNAnVG8xrmMbINuWutrNy?si=Coj3acm8QeODtiRbqPrdjQ
--- neighborhood_mapping_bokeh.py
+++ neighborhood_mapping_bokeh.py
@@ -14,26 +14,21 @@ load_dotenv()
 
 API_KEY = os.getenv('GMAP_API_KEY')
 
-# parcels = gpd.read_file("https://opendata.arcgis.com/datasets/b55c25ae04fc47fc9c188dbbfcd51192_0.geojson")
-parcels = gpd.read_file('parcels_94608.geojson')
 
-parcels['is_key'] = np.where(parcels['MailingAddress'] == "1333 PARK AVE EMERYVILLE CA 94608", 'yes', 'no')
+# parcels = gpd.read_file("https://opendata.arcgis.com/datasets/b55c25ae04fc47fc9c188dbbfcd51192_0.geojson") # full Alameda county data from API:
+
+parcels = gpd.read_file('parcels_94608.geojson') # geojson for a small zip code - much faster than using full county data above
+
+parcels['highlight'] = np.where(parcels['MailingAddress'] == "1333 PARK AVE EMERYVILLE CA 94608", 'yes', 'no') # prepares us to highlight parcels with a particular mailing address (our proxy for ownership)
 
 
-##### dataviz: bokeh ##### 
 
-TOOLTIPS = [
-    ('APN', '@APN'),
-    ('Address', '@SitusAddress'),
-    ('Buildings', '@Buildings'),
-    ('Beds', '@Beds')
-]
-mapper = CategoricalColorMapper(palette=["purple"], factors=['yes'])
-
-geo_source = GeoJSONDataSource(geojson=parcels.to_json())
 
 map_options = GMapOptions(lat=37.84, lng=-122.2835, map_type="hybrid", zoom=15)
-p = gmap(API_KEY, map_options,plot_width=1000, plot_height=1000)
+
+
+p = gmap(API_KEY, map_options,plot_width=1000, plot_height=1000) #creates our map!
+
 p.yaxis.major_tick_line_color = None  # turn off y-axis major ticks
 p.yaxis.minor_tick_line_color = None  # turn off y-axis minor ticks
 p.xaxis.major_tick_line_color = None  # turn off y-axis major ticks
@@ -41,7 +36,20 @@ p.xaxis.minor_tick_line_color = None  # turn off y-axis minor ticks
 p.xaxis.major_label_text_font_size = '0pt'  # turn off x-axis tick labels
 p.yaxis.major_label_text_font_size = '0pt'  # turn off y-axis tick labels
 
-p.patches('xs', 'ys', fill_color = {'field' :'is_key', 'transform' : mapper}, fill_alpha=0.5, source=geo_source)
+
+mapper = CategoricalColorMapper(palette=["purple"], factors=['yes']) # will map "highlight" field yes values to purple
+
+
+geo_source = GeoJSONDataSource(geojson=parcels.to_json()) #map will contain our parcel data
+p.patches('xs', 'ys', fill_color = {'field' :'highlight', 'transform' : mapper}, fill_alpha=0.5, source=geo_source) # creates "patches" (ie shapes) based on our geo data
+
+TOOLTIPS = [
+    ('APN', '@APN'),
+    ('Address', '@SitusAddress'),
+    ('Buildings', '@Buildings'),
+    ('Beds', '@Beds')
+]
+
 p.add_tools( HoverTool(tooltips=TOOLTIPS))
 
 show(p)
