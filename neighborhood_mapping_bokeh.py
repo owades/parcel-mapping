7ed6eb16efbb73dc765eb72aa2e7ff3412a7cd46
Scott Owades <jeremyscottowades@gmail.com>
2020-12-01 11:30:20 -0800

    created address variable

--- neighborhood_mapping_bokeh.py
+++ neighborhood_mapping_bokeh.py
@@ -1,3 +1,4 @@
+# This script will map the properites owned by a given entity (assuming the mailing address is a proxy for ownership)- you just need too input the mailing address .
 from shapely.geometry import Point, Polygon
 import geopandas as gpd
 import geojsonio
@@ -12,21 +13,17 @@ import numpy as np
 from dotenv import load_dotenv
 load_dotenv()
 
-API_KEY = os.getenv('GMAP_API_KEY')
-
+LandlordAddress = "1333 PARK AVE EMERYVILLE CA 94608"
 
 # parcels = gpd.read_file("https://opendata.arcgis.com/datasets/b55c25ae04fc47fc9c188dbbfcd51192_0.geojson") # full Alameda county data from API:
 
 parcels = gpd.read_file('parcels_94608.geojson') # geojson for a small zip code - much faster than using full county data above
 
-parcels['highlight'] = np.where(parcels['MailingAddress'] == "1333 PARK AVE EMERYVILLE CA 94608", 'yes', 'no') # prepares us to highlight parcels with a particular mailing address (our proxy for ownership)
-
-
-
+parcels['highlight'] = np.where(parcels['MailingAddress'] == LandlordAddress, 'yes', 'no') # prepares us to highlight parcels with a particular mailing address (our proxy for ownership)
 
 map_options = GMapOptions(lat=37.84, lng=-122.2835, map_type="hybrid", zoom=15)
 
-
+API_KEY = os.getenv('GMAP_API_KEY')
 p = gmap(API_KEY, map_options,plot_width=1000, plot_height=1000) #creates our map!
 
 p.yaxis.major_tick_line_color = None  # turn off y-axis major ticks
@@ -38,7 +35,6 @@ p.yaxis.major_label_text_font_size = '0pt'  # turn off y-axis tick labels
 
 
 mapper = CategoricalColorMapper(palette=["purple"], factors=['yes']) # will map "highlight" field yes values to purple
-
 
 geo_source = GeoJSONDataSource(geojson=parcels.to_json()) #map will contain our parcel data
 p.patches('xs', 'ys', fill_color = {'field' :'highlight', 'transform' : mapper}, fill_alpha=0.5, source=geo_source) # creates "patches" (ie shapes) based on our geo data
