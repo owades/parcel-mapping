58d7606e066269a65b79d240683cdbd2950b8814
Scott Owades <jeremyscottowades@gmail.com>
2020-12-04 16:03:19 -0800

    script creates csv that counts properties per owner

---
+++ neighborhood_mapping_count.py
@@ -0,0 +1,14 @@
+import geopandas as gpd
+# full Alameda county data from API:
+# parcels = gpd.read_file("https://opendata.arcgis.com/datasets/b55c25ae04fc47fc9c188dbbfcd51192_0.geojson")
+
+# geojson for a small zip code - much faster than using full county data above
+parcels = gpd.read_file('parcels_94608.geojson')
+
+
+#convert count output to dataframe!
+landlord_counts = parcels['MailingAddress'].value_counts().rename_axis('Property Owner Address').reset_index(name='Number of Properties')
+is_major_owner = landlord_counts['Number of Properties'] >= 15
+landlord_counts = landlord_counts[is_major_owner]
+landlord_counts.to_csv('landlords.csv',index=False,header=True)
+
