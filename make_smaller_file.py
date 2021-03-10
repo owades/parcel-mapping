585d1128b3468f1c13a7b196eb5b6b216877b324
Scott Owades <jeremyscottowades@gmail.com>
2021-03-10 15:13:49 -0800

    updated map and table format, filtered out bad data (properties w no street num)

--- make_smaller_file.py
+++ make_smaller_file.py
@@ -1,10 +1,21 @@
 import geopandas as gpd
 
+
 # parcels = gpd.read_file("https://opendata.arcgis.com/datasets/b55c25ae04fc47fc9c188dbbfcd51192_0.geojson")
-# parcels = gpd.read_file('Parcels.geojson')
 parcels = gpd.read_file('parcels_94608.geojson')
-is_neighbor = parcels['SitusZip'] == "94608"
-parcels = parcels[is_neighbor]
-is_owned = parcels['MailingAddress'] == "1333 PARK AVE EMERYVILLE CA 94608"
-parcels = parcels[is_owned]
-parcels.to_file("parcels_small.geojson", driver='GeoJSON')
+# is_neighbor = parcels['SitusZip'] == "94608" 
+# is_neighbor = parcels['SitusCity'] == "OAKLAND" 
+# parcels = parcels[is_neighbor]
+notblank = parcels['SitusStreetNumber'] != "" 
+parcels = parcels[notblank]
+
+# The idea here was to bundle addresses in the same building, but in hindsight I'm seeing that there are many owners for the same building.
+# parcels['Units'] = parcels['Units'] + 1
+# parcels.loc[(parcels.SitusAddress.str.startswith('3300 POWELL ST')),'SitusAddress']='3300 POWELL ST EMERYVILLE CA 94608'
+# parcels['TotalUnits'] = parcels.groupby(['SitusAddress', 'MailingAddress'])['Units'].transform('sum')
+# parcels = parcels.drop_duplicates(subset=['SitusAddress', 'MailingAddress'])
+
+parcels.to_file("parcels_94608.geojson", driver='GeoJSON')
+print("done!")
+# parcels = parcels[parcels['SitusAddress'] == "  SKYLINE BLVD OAKLAND 94608"]
+# print(parcels['SitusStreetNumber'])
--- neighborhood_mapping_count.py
+++ neighborhood_mapping_count.py
@@ -1,14 +1,16 @@
 import geopandas as gpd
+
 # full Alameda county data from API:
 # parcels = gpd.read_file("https://opendata.arcgis.com/datasets/b55c25ae04fc47fc9c188dbbfcd51192_0.geojson")
 
 # geojson for a small zip code - much faster than using full county data above
+# parcels = gpd.read_file('parcels_oakland.geojson')
 parcels = gpd.read_file('parcels_94608.geojson')
 
 
 #convert count output to dataframe!
 landlord_counts = parcels['MailingAddress'].value_counts().rename_axis('Property Owner Address').reset_index(name='Number of Properties')
-is_major_owner = landlord_counts['Number of Properties'] >= 15
+is_major_owner = landlord_counts['Number of Properties'] >= 4
 landlord_counts = landlord_counts[is_major_owner]
-landlord_counts.to_csv('landlords.csv',index=False,header=True)
+landlord_counts.to_csv('landlords_94608.csv',index=False,header=True)
 
