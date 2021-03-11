a9cc8dba50242942933dc0f676de0c5bca20a38f
Scott Owades <jeremyscottowades@gmail.com>
2021-03-11 12:08:05 -0800

    add owner names

--- make_smaller_file.py
+++ make_smaller_file.py
@@ -1,17 +1,22 @@
 import geopandas as gpd
-
+import pandas as pd
 
 # parcels = gpd.read_file("https://opendata.arcgis.com/datasets/b55c25ae04fc47fc9c188dbbfcd51192_0.geojson")
 parcels = gpd.read_file('parcels_94608.geojson')
-# is_neighbor = parcels['SitusZip'] == "94608" 
+is_neighbor = parcels['SitusZip'] == "94608" 
 # is_neighbor = parcels['SitusCity'] == "OAKLAND" 
-# parcels = parcels[is_neighbor]
+parcels = parcels[is_neighbor]
 
 # filter out bad address data:
 notblank = parcels['SitusStreetNumber'] != "" 
 notth = ~parcels['SitusStreetNumber'].str.endswith('TH')
 parcels = parcels[notblank]
 parcels = parcels[notth]
+
+# this is a bit ugly - only run with the below two lines if you are pulling from the API, since the existing file already has the owner column
+# landlords = pd.read_csv('landlord_mapping.csv')
+# parcels = parcels.merge(landlords, how='left',left_on='MailingAddress',right_on='Mailing Address')
+parcels['Owner'] = parcels['Owner'].fillna("TBD")
 
 # The idea here was to bundle addresses in the same building, but in hindsight I'm seeing that there are many owners for the same building.
 # parcels['Units'] = parcels['Units'] + 1
--- neighborhood_mapping_count.py
+++ neighborhood_mapping_count.py
@@ -9,8 +9,10 @@ parcels = gpd.read_file('parcels_94608.geojson')
 
 
 #convert count output to dataframe!
-landlord_counts = parcels['MailingAddress'].value_counts().rename_axis('Property Owner Address').reset_index(name='Number of Properties')
+# landlord_counts = parcels['MailingAddress'].value_counts().rename_axis('Property Owner Address').reset_index(name='Number of Properties')
+landlord_counts = parcels.groupby(["MailingAddress", "Owner"]).size().reset_index(name="Number of Properties")
 is_major_owner = landlord_counts['Number of Properties'] >= 4
 landlord_counts = landlord_counts[is_major_owner]
+landlord_counts = landlord_counts.sort_values(by='Number of Properties',ascending=False)
 landlord_counts.to_csv('landlords_94608.csv',index=False,header=True)
 
