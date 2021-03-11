e9c4f09026259b994b8d2ec516d7c01002c96723
Scott Owades <jeremyscottowades@gmail.com>
2021-03-10 18:39:45 -0800

    filter out more bad addresses

--- make_smaller_file.py
+++ make_smaller_file.py
@@ -6,8 +6,12 @@ parcels = gpd.read_file('parcels_94608.geojson')
 # is_neighbor = parcels['SitusZip'] == "94608" 
 # is_neighbor = parcels['SitusCity'] == "OAKLAND" 
 # parcels = parcels[is_neighbor]
+
+# filter out bad address data:
 notblank = parcels['SitusStreetNumber'] != "" 
+notth = ~parcels['SitusStreetNumber'].str.endswith('TH')
 parcels = parcels[notblank]
+parcels = parcels[notth]
 
 # The idea here was to bundle addresses in the same building, but in hindsight I'm seeing that there are many owners for the same building.
 # parcels['Units'] = parcels['Units'] + 1
